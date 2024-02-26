cfec2068a06e410787a04109cb082004c0c1de5c
saeedforoutan <foroutansaeed@gmail.com>
2024-02-26 21:14:49 +0330

    some commit

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from setuptools import setup
 
 setup(
     name='fibonacci_package',
+    description='A package to calculate the fibonacci series by diferent algorithms',
     version='0.3.1',
     packages=['fibonacci_package'],
     install_requires=[],
     author='Saeed Foroutan',
     author_email='foroutansaeed@live.com',
-    url='https://github.com/SaeedForoutan',
-    summary='A package to calculate the fibonacci series by diferent algorithms'
+    url='https://github.com/SaeedForoutan'
 )
